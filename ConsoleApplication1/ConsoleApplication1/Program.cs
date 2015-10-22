72fb92f748d1e2b6f161f7ead6f2e65e6597603c
cleancodenz2 <cleancodenz2@cleancode.co.nz>
2015-10-22 19:53:19 +1300

    Third change

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -12,6 +12,7 @@ namespace ConsoleApplication1
         {
             Console.WriteLine("Hello world");
             Console.WriteLine("Hello World 2");
+            Console.WriteLine("Hello World 3");
         }
     }
 }
